cb4a632a0d758fc5d6a66adb873c809ae04da699
Trần Đức Nam <v.namtd12@vinai.io>
2024-01-14 17:19:49 +0700

    add pre-commit

--- temi-packaging/usr/local/bin/extract_pdf_content.py
+++ temi-packaging/usr/local/bin/extract_pdf_content.py
@@ -1,15 +1,21 @@
-import PyPDF2
+"""Extract PDF content based on user's input.
+"""
 import sys
+import PyPDF2
+
 
 def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
+    """Extract text from pdf file."""
+    with open(pdf_path, "rb") as file:
         reader = PyPDF2.PdfReader(file)
-        text = ''
+        text = ""
         for page in reader.pages:
-            text += page.extract_text() + '\n'
+            text += page.extract_text() + "\n"
         return text
 
+
 def main():
+    """Main function to extract txt by keyword."""
     if len(sys.argv) != 2:
         print("Usage: extract_text.py <PDF file path>")
         sys.exit(1)
@@ -17,6 +23,7 @@ def main():
     pdf_path = sys.argv[1]
     text = extract_text_from_pdf(pdf_path)
     print(text)
+
 
 if __name__ == "__main__":
     main()
