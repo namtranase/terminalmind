cfdfa29f9c11611da01fd41df070beebbd0c0aad
blebleee <dang.nguyen3@1-DANGNGUYEN3-M1.local>
2024-02-26 15:08:52 +0700

    (dev) python packaging for terminalmind 3

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="terminalmind",
+    version="0.0.1",
+    description="",
+    url="https://github.com/namtranase/terminalmind",
+    author="Nam Duc Tran",
+    author_email="namtranase@gmail.com",
+    license="BSD 2-clause",
+    packages=["terminalmind"],
+    install_requires=["pytest", "pylind", "black", "pre-commit"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
